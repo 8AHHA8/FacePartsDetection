13a2ffbc4e8bc7aabfff8fe7a43b87ce7947bf45
Błażej Homa <blazejh@op.pl>
2024-03-16 23:37:12 +0100

    cosmetics, added title, changed fonts, changed positions of buttons

--- display.py
+++ display.py
@@ -6,13 +6,19 @@ customtkinter.set_default_color_theme("green")
 app = customtkinter.CTk()
 app.geometry("942x440")
 
-app.resizable(False, False)
+app.title("General recognition")
 
-custom_frame = customtkinter.CTkFrame(master=app, width=300, height=240)
-custom_frame.pack(side="left", fill="y")
+custom_frame = customtkinter.CTkFrame(master=app, height=440)
+custom_frame.pack(side="left", fill="both", expand=True)
+custom_frame.master.minsize(width=300, height=0)
+
+title_label = customtkinter.CTkLabel(custom_frame, text="Face parts", font=customtkinter.CTkFont(size=50, weight="bold", family="Papyrus"))
+title_label.place(relx=0.5, rely=0.0, anchor="n")
 
 camera_frame = customtkinter.CTkFrame(master=app)
 camera_frame.pack(side="left", fill="both", expand=True)
 
 camera_canvas = customtkinter.CTkCanvas(master=camera_frame, bg="#1b1b1b")
 camera_canvas.pack(side="left", fill="both", expand=True)
+
+app.resizable(False, False)
--- main.py
+++ main.py
@@ -2,20 +2,19 @@ import numpy as np
 import cv2
 import customtkinter
 from face_parts import eyes, mouth, nose, face
-from PIL import Image, ImageTk
 
 from display import app, custom_frame, camera_canvas
 
-face = customtkinter.CTkButton(master=custom_frame, text="Face detection", command=face)
-face.place(relx=0.5, rely=0.2, anchor=customtkinter.CENTER)
+face = customtkinter.CTkButton(master=custom_frame, text="Face detection", command=face, font=customtkinter.CTkFont(size=20, weight="bold", family="Papyrus"))
+face.place(relx=0.5, rely=0.25, anchor="center")
 
-eyes = customtkinter.CTkButton(master=custom_frame, text="Eyes", command=eyes)
-eyes.place(relx=0.5, rely=0.4, anchor="center")
+eyes = customtkinter.CTkButton(master=custom_frame, text="Eyes", command=eyes, font=customtkinter.CTkFont(size=20, weight="bold", family="Papyrus"))
+eyes.place(relx=0.5, rely=0.45, anchor="center")
 
-Mouth = customtkinter.CTkButton(master=custom_frame, text="Mouth", command=mouth)
-Mouth.place(relx=0.5, rely=0.6, anchor="center")
+Mouth = customtkinter.CTkButton(master=custom_frame, text="Mouth", command=mouth, font=customtkinter.CTkFont(size=20, weight="bold", family="Papyrus"))
+Mouth.place(relx=0.5, rely=0.65, anchor="center")
 
-nose = customtkinter.CTkButton(master=custom_frame, text="Nose", command=nose)
-nose.place(relx=0.5, rely=0.8, anchor="center")
+nose = customtkinter.CTkButton(master=custom_frame, text="Nose", command=nose, font=customtkinter.CTkFont(size=20, weight="bold", family="Papyrus"))
+nose.place(relx=0.5, rely=0.86, anchor="center")
 
 app.mainloop()
